3e4298d9fe110430a1942cb5e85e1ad3d81bf6aa
Nikulaev-Viktor <viktor_1611@mail.ru>
2024-06-03 23:39:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from src.func import json_encode, last_five_operations, get_data_format
 data = json_encode()
 data = last_five_operations(data)
 get_data_format(data)
-#for i in last_five_operations(data)[5]:
 print(get_data_format(data))
