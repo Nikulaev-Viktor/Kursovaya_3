7a17391ef23814a705b0e4254e5cb8b066fbc1eb
Viktor Nikulaev <viktor_1611@mail.ru>
2024-06-04 22:34:13 +0300

    Error correction

--- src/func.py
+++ src/func.py
@@ -1,5 +1,4 @@
 import json
-from datetime import datetime
 
 
 def json_encode():
@@ -11,16 +10,9 @@ def json_encode():
 
 def last_five_operations(data):
     """Сортируем и выводим пять последних операций"""
-    new_operation = [operation for operation in data if "state" in operation and operation["state"] == "EXECUTED"] #объед две функции
+    new_operation = [operation for operation in data if "state" in operation and operation["state"] == "EXECUTED"]
     sorted_operations = sorted(new_operation, key=lambda x: x["date"], reverse=True)
-    return sorted_operations[:4]
-    # for operations in data:
-    #     if "state" in operations and operations["state"] == "EXECUTED":
-    #         new_operation.append(operations)
-    # sorted_operations = sorted(new_operation, key=lambda x: x["date"], reverse=True)
-    # return sorted_operations[-3:]
-
-# Не работает, выводит либо 4, либо 6
+    return sorted_operations[:5]
 
 
 def format_date(date):
@@ -34,7 +26,7 @@ def format_card(card):
     card_number = card.pop()
     card_name = " ".join(card)
     if card_name.lower() == "счет":
-        number_secret = "** " + card_number[-4:]
+        number_secret = "**" + card_number[-4:]
     else:
         number_secret = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
     return f"{card_name} {number_secret}"
@@ -43,7 +35,7 @@ def format_card(card):
 def get_data_format(data):
     """"Форматируем данные под ТЗ"""
     for operation in data:
-        if "from" in operation:
+        try:
             date = format_date(operation["date"])
             operation_a = operation["description"]
             from_who = format_card(operation["from"])
@@ -51,5 +43,5 @@ def get_data_format(data):
             sum_trans = operation["operationAmount"]["amount"]
             currency = operation["operationAmount"]["currency"]["name"]
             print(f"{date} {operation_a}\n{from_who} -> {to_who}\n{sum_trans} {currency}")
-        else:
-            pass
+        except KeyError:
+            continue
